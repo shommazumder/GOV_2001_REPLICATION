ee6990538a2db9cd34b9dc6399f77dc521aee061
Shom Mazumder <shom.mazumder@gmail.com>
2016-04-19 00:58:35 -0400

    init final proj

---
+++ 02_Analysis/Final_Paper/batch.R
@@ -0,0 +1,26 @@
+#######################################################################
+##AUTHORS: JEREMY BOWLES, JESSIE BULLOCK, AND SHOM MAZUMDER
+##DATE CREATED: 04/19/2016
+##DATE LAST UPDATED: 04/19/2016
+##PURPOSE: THIS SCRIPT REPLICATES THE ANALYSIS IN BOWLES ET AL (2016)
+#######################################################################
+
+####PACKAGES####
+required.packages <- c('foreign','ggplot2','stargazer','sandwich','multiwayvcov',
+                       'MASS','rms','lmtest','ordinal','aod','glmnet')
+if(sum(required.packages %in% rownames(installed.packages()))!=length(required.packages)){
+  install.packages(required.packages[which(!(required.packages %in% rownames(installed.packages())))])
+}
+lapply(required.packages, library, character.only = TRUE)
+
+####SETUP####
+##Set working directory
+setwd(getwd())#make sure to open the rproj file so you don't have directory structure problems
+
+####MATCHING####
+
+####MECHANISMS####
+
+####VARIABLE SELECTION####
+
+####MISCELLANEOUS####
